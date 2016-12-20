3c6e42cdb9678aae4cd9bfeec32509bd7abfdbd8
Megan Kaluza <megan.kaluza@gmail.com>
2016-12-20 14:48:28 -0800

    add jquery image grow effects

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.box').click(function(){
+     $('.box').toggleClass('clicked');
+    //  $('.galleryTwo').hide();
+    //  $('.galleryThree').hide();
+  });
+});
