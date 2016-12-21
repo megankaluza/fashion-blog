653eefb0a70d4486441f64cb930e9be3966903a4
Megan Kaluza <megan.kaluza@gmail.com>
2016-12-21 09:36:58 -0800

    add modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
   $('.box').click(function(){
      $('.box').toggleClass('clicked');
-    //  $('.galleryTwo').blur();
-    //  $('.galleryThree').blur();
   });
 });
